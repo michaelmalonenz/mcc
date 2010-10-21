7d83707bc8ebbe1c24231ed8ddfe25aecedc8948
Michael Malone <michael.malone@taitradio.com>
2010-10-21 20:40:21 +1300

    Combined two loops which didn't have a solid reason for being separate and removed the memory leak

--- mcc.c
+++ mcc.c
@@ -15,16 +15,24 @@ static unsigned short num_files = 0;
 int main(int argc, char **argv)
 {
 	int i;
+
+	if (argc <= 1)
+	{
+		mcc_Error("No input files\n");
+	}
+
 	for(i = 1; i < argc; i++)
 	{
 		input_files[i-1] = (char *) malloc(sizeof(char) * strlen(argv[i]));
 		strncpy(input_files[i-1], argv[i], strlen(argv[i]));
 		num_files++;
+		mcc_ParseFile(input_files[i]);
 	}
+
 
 	for(i = 0; i < num_files; i++)
 	{
-		mcc_ParseFile(input_files[i]);
+		free(input_files[i]);
 	}
 
 	return 0;
