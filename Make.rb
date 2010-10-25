d4baa4376b8199fe7978ce2443e2366df08a2759
Michael Malone <michael.malone@taitradio.com>
2010-10-25 23:05:47 +1300

    minimal test building setup

--- Make.rb
+++ Make.rb
@@ -3,6 +3,11 @@
 require 'fileutils'
 
 BIN_DIR = 'bin'
+TEST_BIN_DIR = "#{BIN_DIR}/test"
+
+SRC_DIR = Dir.pwd
+TEST_SRC = "#{SRC_DIR}/test"
+
 TEMP_STDERR_FILE = 'stderr'
 
 CFLAGS = "-Wall -Wextra -Werror -g -ggdb3"
@@ -28,6 +33,18 @@ def clean()
    FileUtils.rm_rf(BIN_DIR)
 end
 
+def compile_a_directory(input_dir, out_dir)
+   o_files = []
+   Dir.chdir(input_dir) do
+      Dir.glob("*.c").each do |file|
+         o_file = c_to_o(file)
+         o_files << o_file
+         run_command("#{CC} #{CFLAGS} -c #{file} -o #{out_dir}/#{o_file}", 
+                     "Compilation of #{input_dir}/#{file} failed...")
+      end
+   end
+end
+
 if $0 == __FILE__ then
 
    ARGV.each do |arg|
@@ -38,15 +55,14 @@ if $0 == __FILE__ then
    end
 
    FileUtils.mkdir(BIN_DIR) unless FileTest.exist?(BIN_DIR)
-   o_files = []
-   Dir.glob("*.c").each do |file|
-      o_file = c_to_o(file)
-      o_files << o_file
-      run_command("#{CC} #{CFLAGS} -c #{file} -o #{BIN_DIR}/#{o_file}", 
-                  "Compilation of #{file} failed...")
-   end
+   FileUtils.mkdir(TEST_BIN_DIR) unless FileTest.exist?(TEST_BIN_DIR)
+
+   compile_a_directory(SRC_DIR, BIN_DIR)
+   compile_a_directory(TEST_SRC_DIR, TEST_BIN_DIR)
+
    Dir.chdir(BIN_DIR) do
       run_command("#{CC} #{o_files.join(' ')} -o mcc", "Linking mcc Failed...")
    end
 
+   #link the test executables
 end
