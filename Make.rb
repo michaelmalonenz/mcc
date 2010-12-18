4bfec0dfdc94bc7633dd9196cddd38e0e93ec70d
Michael Malone <michael.malone@taitradio.com>
2010-12-19 12:19:55 +1300

    added a dynamic linker, which allows the tests to link.  Don't know why the tests don't automagically run

--- Make.rb
+++ Make.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/ruby -w
 
 require 'fileutils'
+require 'set'
 
 BIN_DIR = "#{File.expand_path(File.dirname($0))}/bin"
 TEST_BIN_DIR = "#{BIN_DIR}/test"
@@ -13,6 +14,58 @@ TEMP_STDERR_FILE = 'stderr'
 CFLAGS = "-Wall -Wextra -Werror -g -ggdb3 -Os"
 
 CC = "/usr/bin/gcc"
+
+class Linker
+
+   FILE_SYMBOL_REGEX = %r{^(.*?):[a-f0-9]*\s+\w\s+(\w+?)$}i
+
+   def initialize
+      @symbols = {}
+   end
+
+   # For every file in the directory which ends in .o
+   # find which symbols they define and add them to the class's hash
+   def discover_defined_symbols(dir)
+      files = nil
+      dir = File.expand_path(dir)
+      Dir.chdir(dir) do
+         files = Dir.glob('*.o')
+         `nm --extern-only --defined-only -A #{files.join(' ')}`.each_line do |line|
+            if (line =~ FILE_SYMBOL_REGEX)
+               @symbols[$2] = File.expand_path($1)
+            end
+         end
+      end
+   end
+
+   def discover_required_files(o_file)
+      o_file = File.expand_path(o_file)
+      needed_symbols = find_required_symbols(o_file)
+      required_files = Set.new
+      unsearched_files = [o_file]
+      while(unsearched_files.size > 0) do
+         file = unsearched_files.shift
+         next if (required_files.include? file)
+         find_required_symbols(file).each do |sym|
+            unsearched_files << @symbols[sym] if (@symbols.has_key?(sym))
+         end
+         required_files << file
+      end
+      return required_files.to_a
+   end
+
+   private
+   def find_required_symbols(o_file)
+      symbols = Set.new()
+      `nm --extern-only --undefined-only -A #{o_file}`.each_line do |line|
+         if (line =~ FILE_SYMBOL_REGEX)
+            symbols << $2
+         end
+      end
+      return symbols
+   end
+
+end
 
 def c_to_o(file)
    return file.gsub(%r{ \.c$ }x,".o")
@@ -46,11 +99,8 @@ def compile_a_directory(input_dir, out_dir)
    end
 end
 
-def find_dependencies(main_o_file)
-   
-end
 
-$0 == __FILE__ then
+if $0 == __FILE__ then
 
    ARGV.each do |arg|
       if arg =~ %r{--?c(?:l(?:e(?:a(?:n)?)?)?)?}ix
@@ -59,33 +109,34 @@ $0 == __FILE__ then
       end
    end
 
-   FileUtils.mkdir(BIN_DIR) unless FileTest.exist?(BIN_DIR)
-   FileUtils.mkdir(TEST_BIN_DIR) unless FileTest.exist?(TEST_BIN_DIR)
+   FileUtils.mkdir(BIN_DIR, :mode => 0775) unless FileTest.exist?(BIN_DIR)
+   FileUtils.mkdir(TEST_BIN_DIR, :mode => 0775) unless FileTest.exist?(TEST_BIN_DIR)
 
    compile_a_directory(SRC_DIR, BIN_DIR)
    compile_a_directory(TEST_SRC_DIR, TEST_BIN_DIR)
 
-   bin_o_files = nil
+   linker = Linker.new()
+   linker.discover_defined_symbols(BIN_DIR)
+
    Dir.chdir(BIN_DIR) do
-      bin_o_files = Dir.glob('*.o')
-      run_command("#{CC} #{bin_o_files.join(' ')} -o mcc", "Linking mcc Failed...")
+      dependencies = linker.discover_required_files('mcc.o')
+      run_command("#{CC} #{dependencies.join(' ')} -o mcc", "Linking mcc Failed...")
    end
 
-   # If anything necessary to link a unit test lives in here, it must be factored out
-   bin_o_files.delete('mcc.o')
+   Dir.chdir(TEST_BIN_DIR) do
+      Dir.glob("test_*.o").each do |test_exe_o|
+         test_exe = test_exe_o.gsub(/\.o$/, '')
+         dependencies = linker.discover_required_files(test_exe_o)
+         run_command("#{CC} #{dependencies.join(' ')} -o #{test_exe}", 
+                     "Linking #{test_exe} Failed...")
+      end
+   end
 
-#   Dir.chdir(TEST_BIN_DIR) do
-#      Dir.glob("test_*.o").each do |test_exe_o|
-#         test_exe = test_exe_o.gsub(/\.o$/, '')
-#         run_command("#{CC} #{BIN_DIR}/#{bin_o_files.join(" #{BIN_DIR}/")} #{test_exe_o} -o #{test_exe}", 
-#                     "Linking #{test_exe} Failed...")
-#      end
-#   end
-
-#   Dir.new(TEST_BIN_DIR).each do |file|
-#      if !FileTest.directory?(file) && FileTest.executable?(file)
-#         run_command(file, "#{File.basename(file)} failed to run correctly!")
-#      end
-#   end
+   Dir.new(TEST_BIN_DIR).each do |file|
+      if !FileTest.directory?(file) && FileTest.executable?(file)
+         #Why the hell doesn't this work????
+         run_command(file, "#{File.basename(file)} failed to run correctly!")
+      end
+   end
 
 end
