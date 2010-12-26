4acec4e61ac3b10d510c6a5aaf80c40b289584ca
Michael Malone <michael.malone@taitradio.com>
2010-12-27 09:59:56 +1300

    Enhanced the macro test to show that it is the cause of the memory leakage, still need to fix the memory leakage

--- test/test_Macros.c
+++ test/test_Macros.c
@@ -10,6 +10,15 @@
 
 #define MACRO_NAME "EAT_ME"
 
+#define NUM_BULK_MACROS 10
+const char *test_Macros[NUM_BULK_MACROS] = { "FIRST_MACRO", "SECOND_MACRO", "THIRD_MACRO", 
+                                             "FOURTH_MACRO", "FIFTH_MACRO", "SIXTH_MACRO",
+                                             "SEVENTH_MACRO", "EIGHTH_MACRO", "NINETH_MACRO",
+                                             "TENTH_MACRO" };
+
+char *test_MacroValues[NUM_BULK_MACROS] = { "1", "two", "1+2", "(FIRST_MACRO) + 3", "5", "2*3",
+                                                  "7", "8", "(2*4+1)", "1+2*3+3" };
+
 static void test_Define(void)
 {
    printf("Testing Define...");
@@ -33,10 +42,23 @@ static void test_Undefine(void)
    printf("ok\n");
 }
 
+
+static void test_BulkMacros(void)
+{
+   int i;
+   printf("Testing Bulk Macro Definitions...");
+   for(i = 0; i < NUM_BULK_MACROS; i++)
+   {
+      mcc_DefineMacro(test_Macros[i], test_MacroValues[i]);
+   }
+   mcc_DeleteAllMacros();
+}
+
 int main(void)
 {
    test_Define();
    test_Find();
    test_Undefine();
+   test_BulkMacros();
    return 0;
 }
