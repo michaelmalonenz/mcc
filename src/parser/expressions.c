c17cf10b398214b439d412b92cf7842fea52cc1e
Michael Malone <michael.j.b.malone@gmail.com>
2020-11-04 15:40:06 +1300

    Parsing assignment expressions

--- src/parser/expressions.c
+++ src/parser/expressions.c
@@ -13,65 +13,44 @@ mcc_ASTNode_t *parse_constant_expression(mcc_AST_t *tree)
  * <conditional-expression> ::= <logical-or-expression>
  *                            | <logical-or-expression> ? <expression> : <conditional-expression>
  */
-mcc_ASTNode_t *parse_conditional_expression(mcc_AST_t UNUSED(*tree))
-{
-    return NULL;
-}
+mcc_ASTNode_t *parse_conditional_expression(mcc_AST_t UNUSED(*tree)) { return NULL; }
 
 /**
  * <logical-or-expression> ::= <logical-and-expression>
  *                           | <logical-or-expression> || <logical-and-expression>
  */
-mcc_ASTNode_t *parse_logical_or_expression(mcc_AST_t UNUSED(*tree))
-{
-    return NULL;
-}
+mcc_ASTNode_t *parse_logical_or_expression(mcc_AST_t UNUSED(*tree)) { return NULL; }
 
 /**
  * <logical-and-expression> ::= <inclusive-or-expression>
  *                            | <logical-and-expression> && <inclusive-or-expression>
  */
-mcc_ASTNode_t *parse_logical_and_expression(mcc_AST_t UNUSED(*tree))
-{
-    return NULL;
-}
+mcc_ASTNode_t *parse_logical_and_expression(mcc_AST_t UNUSED(*tree)) { return NULL; }
 
 /**
  * <inclusive-or-expression> ::= <exclusive-or-expression>
  *                             | <inclusive-or-expression> | <exclusive-or-expression>
  */
-mcc_ASTNode_t *parse_inclusive_or_expression(mcc_AST_t UNUSED(*tree))
-{
-    return NULL;
-}
+mcc_ASTNode_t *parse_inclusive_or_expression(mcc_AST_t UNUSED(*tree)) { return NULL; }
 
 /**
  * <exclusive-or-expression> ::= <and-expression>
  *                             | <exclusive-or-expression> ^ <and-expression>
  */
-mcc_ASTNode_t *parse_exclusive_or_expression(mcc_AST_t UNUSED(*tree))
-{
-    return NULL;
-}
+mcc_ASTNode_t *parse_exclusive_or_expression(mcc_AST_t UNUSED(*tree)) { return NULL; }
 
 /**
  * <and-expression> ::= <equality-expression>
  *                    | <and-expression> & <equality-expression>
  */
-mcc_ASTNode_t *parse_and_expression(mcc_AST_t UNUSED(*tree))
-{
-    return NULL;
-}
+mcc_ASTNode_t *parse_and_expression(mcc_AST_t UNUSED(*tree)) { return NULL; }
 
 /**
  * <equality-expression> ::= <relational-expression>
  *                         | <equality-expression> == <relational-expression>
  *                         | <equality-expression> != <relational-expression>
  */
-mcc_ASTNode_t *parse_equality_expression(mcc_AST_t UNUSED(*tree))
-{
-    return NULL;
-}
+mcc_ASTNode_t *parse_equality_expression(mcc_AST_t UNUSED(*tree)) { return NULL; }
 
 /**
  * <relational-expression> ::= <shift-expression>
@@ -80,30 +59,21 @@ mcc_ASTNode_t *parse_equality_expression(mcc_AST_t UNUSED(*tree))
  *                           | <relational-expression> <= <shift-expression>
  *                           | <relational-expression> >= <shift-expression>
  */
-mcc_ASTNode_t *parse_relational_expression(mcc_AST_t UNUSED(*tree))
-{
-    return NULL;
-}
+mcc_ASTNode_t *parse_relational_expression(mcc_AST_t UNUSED(*tree)) { return NULL; }
 
 /**
  * <shift-expression> ::= <additive-expression>
  *                      | <shift-expression> << <additive-expression>
  *                      | <shift-expression> >> <additive-expression>
  */
-mcc_ASTNode_t *parse_shift_expression(mcc_AST_t UNUSED(*tree))
-{
-    return NULL;
-}
+mcc_ASTNode_t *parse_shift_expression(mcc_AST_t UNUSED(*tree)) { return NULL; }
 
 /**
  * <additive-expression> ::= <multiplicative-expression>
  *                         | <additive-expression> + <multiplicative-expression>
  *                         | <additive-expression> - <multiplicative-expression>
  */
-mcc_ASTNode_t *parse_additive_expression(mcc_AST_t UNUSED(*tree))
-{
-    return NULL;
-}
+mcc_ASTNode_t *parse_additive_expression(mcc_AST_t UNUSED(*tree)) { return NULL; }
 
 /**
  * <multiplicative-expression> ::= <cast-expression>
@@ -111,19 +81,13 @@ mcc_ASTNode_t *parse_additive_expression(mcc_AST_t UNUSED(*tree))
  *                               | <multiplicative-expression> / <cast-expression>
  *                               | <multiplicative-expression> % <cast-expression>
  */
-mcc_ASTNode_t *parse_multiplicative_expression(mcc_AST_t UNUSED(*tree))
-{
-    return NULL;
-}
+mcc_ASTNode_t *parse_multiplicative_expression(mcc_AST_t UNUSED(*tree)) { return NULL; }
 
 /**
  * <cast-expression> ::= <unary-expression>
  *                     | ( <type-name> ) <cast-expression>
  */
-mcc_ASTNode_t *parse_cast_expression(mcc_AST_t UNUSED(*tree))
-{
-    return NULL;
-}
+mcc_ASTNode_t *parse_cast_expression(mcc_AST_t UNUSED(*tree)) { return NULL; }
 
 /**
  * <unary-expression> ::= <postfix-expression>
@@ -133,10 +97,7 @@ mcc_ASTNode_t *parse_cast_expression(mcc_AST_t UNUSED(*tree))
  *                      | sizeof <unary-expression>
  *                      | sizeof <type-name>
  */
-mcc_ASTNode_t *parse_unary_expression(mcc_AST_t UNUSED(*tree))
-{
-    return NULL;
-}
+mcc_ASTNode_t *parse_unary_expression(mcc_AST_t UNUSED(*tree)) { return NULL; }
 
 /**
  * <postfix-expression> ::= <primary-expression>
@@ -147,10 +108,7 @@ mcc_ASTNode_t *parse_unary_expression(mcc_AST_t UNUSED(*tree))
  *                        | <postfix-expression> ++
  *                        | <postfix-expression> --
  */
-mcc_ASTNode_t *parse_postfix_expression(mcc_AST_t UNUSED(*tree))
-{
-    return NULL;
-}
+mcc_ASTNode_t *parse_postfix_expression(mcc_AST_t UNUSED(*tree)) { return NULL; }
 
 /**
  * <primary-expression> ::= <identifier>
@@ -158,10 +116,7 @@ mcc_ASTNode_t *parse_postfix_expression(mcc_AST_t UNUSED(*tree))
  *                        | <string>
  *                        | ( <expression> )
  */
-mcc_ASTNode_t *parse_primary_expression(mcc_AST_t UNUSED(*tree))
-{
-    return NULL;
-}
+mcc_ASTNode_t *parse_primary_expression(mcc_AST_t UNUSED(*tree)) { return NULL; }
 
 /**
  * <constant> ::= <integer-constant>
@@ -169,27 +124,37 @@ mcc_ASTNode_t *parse_primary_expression(mcc_AST_t UNUSED(*tree))
  *              | <floating-constant>
  *              | <enumeration-constant>
  */
-mcc_ASTNode_t *parse_constant(mcc_AST_t UNUSED(*tree))
-{
-    return NULL;
-}
+mcc_ASTNode_t *parse_constant(mcc_AST_t UNUSED(*tree)) { return NULL; }
 
 /**
  * <expression> ::= <assignment-expression>
  *                | <expression> , <assignment-expression>
  */
-mcc_ASTNode_t *parse_expression(mcc_AST_t UNUSED(*tree))
-{
-    return NULL;
-}
+mcc_ASTNode_t *parse_expression(mcc_AST_t UNUSED(*tree)) { return NULL; }
 
 /**
  * <assignment-expression> ::= <conditional-expression>
  *                           | <unary-expression> <assignment-operator> <assignment-expression>
  */
-mcc_ASTNode_t *parse_assignment_expression(mcc_AST_t UNUSED(*tree))
+mcc_ASTNode_t *parse_assignment_expression(mcc_AST_t *tree)
 {
-    return NULL;
+    mcc_ASTNode_t *node = parse_conditional_expression(tree);
+    if (node != NULL) return node;
+
+    mcc_ASTNode_t *unary_node = parse_unary_expression(tree);
+    node = parse_assignment_operator(tree);
+    if (node != NULL)
+    {
+        node->left = unary_node;
+        node->right = parse_assignment_expression(tree);
+    }
+    else if (unary_node != NULL)
+    {
+        mcc_PrettyErrorToken(tree->currentToken, "Expected an assignment operator, but got '%s'\n",
+                             tree->currentToken->text);
+    }
+
+    return node;
 }
 
 /**
